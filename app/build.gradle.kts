b09991dcb5b5bad6bbf3b149c9f21640def117d5
Yeider15 <ye.catari@duocuc.cl>
2025-11-05 11:33:31 -0300

    Se agregar y corrigen dependedencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pasteleriaapp_p2"
+    namespace = "com.example.pasteleria"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.pasteleriaapp_p2"
+        applicationId = "com.example.pasteleria"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
